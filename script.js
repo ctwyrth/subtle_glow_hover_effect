e1a3502e14400d7e1966dcde424d0b3c03d51367
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-02-16 10:07:40 -0800

    3rd Commit, finished tutorial, glows work

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const handleOnMouseMove = (e) => {
-   const { currentTarget: target } = e;
-}
+// const handleOnMouseMove = e => {
+//    const { currentTarget: target } = e;
+   
+//    const rect = target.getBoundingClientRect(),
+//       x = e.clientX - rect.left,
+//       y = e.clientY - rect.top;
+   
+//    target.style.setProperty("--mouse-x", `${x}px`);
+//    target.style.setProperty("--mouse-y", `${y}px`);
+// }
 
-for (let card of document.querySelectorAll('.card')) {
-   card.onmousemove = (e) => handleOnMouseMove(e);
+// for (let card of document.querySelectorAll('.card')) {
+//    card.onmousemove = e => handleOnMouseMove(e);
+// }
+
+document.getElementById("cards").onmousemove = e => {
+   for (let card of document.querySelectorAll(".card")) {
+      const rect = card.getBoundingClientRect(),
+         x = e.clientX - rect.left,
+         y = e.clientY - rect.top;
+      
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+   }
 }
